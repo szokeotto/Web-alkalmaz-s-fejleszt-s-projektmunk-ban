505722af01fdc2000ed7697fb792b5668586a67b
Szőke Ottó <szoke.otto@gmail.com>
2025-04-04 14:48:34 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+//Erőforrások
+require "config/connect.php";/** @var $link mysqli */
+require "config/settings.php";
+require "config/functions.php";//saját eljárások
+//var_dump($_SESSION);
+session_start();//Munkafolyamat indítása
+
+//A $_SESSION szuperglobális tömb
+$loginMessage = '<h1>Kérjük írja be a bejelentkezési adatokat:</h1>';
+
+if (!empty($_POST)) {
+    if (login()) {
+        //irány az admin/index
+        header('location:view/index.php');
+        exit;
+    } else {//nem volt ilyen
+        $loginMessage = '<span class="error">Nincs ilyen email/jelszó páros!</span>';
+    }
+}
+?><!doctype html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP Vizsga Admin felület - bejelentkezés</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+        }
+
+        form {
+            max-width: 600px;
+            margin: 1em auto;
+        }
+
+        label {
+            display: flex;
+            flex-direction: column;
+        }
+
+        .error {
+            font-size: 0.9em;
+            color: red;
+            font-style: italic;
+        }
+    </style>
+</head>
+<body>
+<form method="post">
+    <?php echo $loginMessage; ?>
+    <label>
+        <span>Email</span>
+        <input type="text" name="email" placeholder="email@cim.hu"
+               value="<?php echo filter_input(INPUT_POST, 'email'); ?>">
+    </label>
+    <label>
+        <span>Jelszó</span>
+        <input type="password" name="password" placeholder="******" value="">
+    </label>
+    <button>Belépés</button>
+    <a href="view/registration.php">Regisztráció</a>
+</form>
+</body>
+</html>
