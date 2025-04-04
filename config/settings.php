0d54bf3b4fabdf1c0be3f9639305a1ec4cb815e8
Szőke Ottó <szoke.otto@gmail.com>
2025-04-04 15:34:28 +0200

    Add files via upload

---
+++ config/settings.php
@@ -0,0 +1,14 @@
+<?php
+const SECRET_KEY = 'S3cR3t_K3Y!%"+';//tikosításhoz
+const MODULE_DIR = '../view/';//modulok mappája
+const MODULE_EXT = '.php';//modulfileok kiterjesztése
+
+//admin menü
+const ADMIN_MENU = [
+    0 => [
+        'title' => 'Kód feltöltése',
+        'module' => 'upload',
+        'icon' => 'fas fa-tachometer-alt'
+    ],
+
+];
